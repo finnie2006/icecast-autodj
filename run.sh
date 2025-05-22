e92980c43b1da6c94d7c53d8c30ee8c7f46c143c
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2025-05-22 11:14:53 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Start Icecast in the background
+/usr/bin/icecast2 -b -c "/home/container/icecast.xml"
+
+# Wait a few seconds to ensure Icecast is up
+sleep 5
+
+# Start Liquidsoap
+/liquidsoap --verbose /home/container/radio.liq
