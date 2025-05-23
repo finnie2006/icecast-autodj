23ff0f92dd031be186672bd6cbdaf60f93beb3a4
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2025-05-23 10:35:29 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,13 @@
 set -e
 
 # Start Icecast in the background
+echo "[*] Starting Icecast2..."
 /usr/bin/icecast2 -b -c "/home/container/icecast.xml"
 
 # Wait a few seconds to ensure Icecast is up
-sleep 5
+echo "[*] Waiting for Icecast to initialize..."
+sleep 3
 
 # Start Liquidsoap
+echo "[*] Starting Liquidsoap..."
 liquidsoap --verbose /home/container/radio.liq
